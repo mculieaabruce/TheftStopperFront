b252ef8968a3ad4191b5eba59526fe072c621b16
Andre Rios <ariosnoda18@gmail.com>
2024-06-18 21:42:06 -0500

    commit4AYAYAY

--- src/app/Components/servicios/foro/foro.component.ts
+++ src/app/Components/servicios/foro/foro.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicacionComponent } from './publicacion/publicacion.component';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-foro',
   standalone: true,
-  imports: [PublicacionComponent,RouterOutlet],
+  imports: [PublicacionComponent,RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './foro.component.html',
   styleUrl: './foro.component.css'
 })
--- src/app/Components/servicios/foro/publicacion/publicacion.component.ts
+++ src/app/Components/servicios/foro/publicacion/publicacion.component.ts
@@ -1,18 +1,28 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
+import { MatTableDataSource } from '@angular/material/table';
+import { publicacion } from '../../../../Models/publicacion';
+import { PublicacionService } from '../../../../Services/publicacion.service';
 
 @Component({
   selector: 'app-publicacion',
   standalone: true,
-  imports: [NgFor, MatCardModule,MatButtonModule,MatIconModule,MatInput,MatFormFieldModule],
+  imports: [NgFor, MatCardModule,MatButtonModule,MatIconModule,MatInput,MatFormFieldModule,CommonModule],
   templateUrl: './publicacion.component.html',
   styleUrl: './publicacion.component.css'
 })
-export class PublicacionComponent {
-
+export class PublicacionComponent implements OnInit {
+  publicaciones:publicacion[]=[]
+  dataSource:MatTableDataSource<publicacion> = new MatTableDataSource()
+  constructor(private pS: PublicacionService) {}
+  ngOnInit(): void {
+    this.pS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
 }
