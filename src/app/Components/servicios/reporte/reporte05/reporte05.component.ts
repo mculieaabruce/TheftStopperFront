f5755352e5f771102860b7b0c60de15ffb8c2e95
Franco <franco.lazo19@outlook.com>
2024-06-22 22:26:48 -0500

    anb

--- src/app/Components/servicios/reporte/reporte05/reporte05.component.ts
+++ src/app/Components/servicios/reporte/reporte05/reporte05.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { CasoService } from '../../../../Services/caso.service';
 
 @Component({
   selector: 'app-reporte05',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './reporte05.component.html',
-  styleUrl: './reporte05.component.css'
+  styleUrl: './reporte05.component.css',
 })
-export class Reporte05Component {
+export class Reporte05Component implements OnInit {
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: string[] = [];
+  //barChartType: ChartType = 'pie';
+  barChartType: ChartType = 'doughnut';
+  //barChartType: ChartType = 'line';
+  //barChartType: ChartType = 'bar';
+  //barChartType: ChartType = 'polarArea';
 
+  barChartLegend = true;
+  barChartData: ChartDataset[] = [];
+  constructor(private cS: CasoService) {}
+  ngOnInit(): void {
+    this.cS.getCasosxDistrito().subscribe((data)=>{
+      this.barChartLabels =data.map((item)=>item.distrito)
+      this.barChartData = [
+        {
+          data: data.map((item) => item.numero_de_casos),
+          label: 'Numero de casos',
+          backgroundColor: [
+            '#0094d3',
+            '#4169c7',
+            
+          ],
+          borderColor: 'rgba(173, 216, 230, 1)',
+          borderWidth: 1,
+        }
+      ]
+
+    })
+  }
 }
